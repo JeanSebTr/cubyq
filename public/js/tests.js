9e79d698f4f8aba1879fa7291cac3b0a3b932a01
Melanie Bilodeau <tiger_panthere@MacBook-Pro-de-Melanie-Bilodeau.local>
2012-06-15 18:23:28 -0400

    modif index test

---
+++ public/js/tests.js
@@ -0,0 +1,10 @@
+// Author: Melanie Bilodeau, m_bilodeau@hotmail.ca, vendredi 15 juin 2012
+
+window.onload=configurer;
+
+function configurer()
+{
+	document.getElementById("cont_hist").className ="";
+	document.getElementById("cont_htp").className ="invisible";
+	document.getElementById("cont_hiScr").className ="invisible";
+}
