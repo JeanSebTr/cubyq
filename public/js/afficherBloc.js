db37583725aee375421932cc4277ea98ffa4407d
Jean-Sébastien Tremblay <jean-seb@total-itech.com>
2012-06-17 09:20:23 -0400

    patlake

--- public/js/afficherBloc.js
+++ public/js/afficherBloc.js
@@ -1,6 +1,7 @@
 var Game, socket;
 	function changerdiv0()
 	{
+		document.getElementById("cont_desc").style.display = "none";
 		document.getElementById("cont_play").className = "";
 		document.getElementById("cont_hist").className = "invisible";
 		document.getElementById("cont_htp").className = "invisible";
@@ -17,6 +18,7 @@ var Game, socket;
 	}
 	function changerdiv1()
 	{
+		document.getElementById("cont_desc").style.display = "block";
 		document.getElementById("cont_play").className = "invisible";
 		document.getElementById("cont_hist").className = "";
 		document.getElementById("cont_htp").className = "invisible";
@@ -28,6 +30,7 @@ var Game, socket;
 	
 	function changerdiv2()
 	{
+		document.getElementById("cont_desc").style.display = "block";
 		document.getElementById("cont_play").className = "invisible";
 		document.getElementById("cont_hist").className = "invisible";
 		document.getElementById("cont_htp").className = "";
